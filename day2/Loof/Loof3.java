effd442bbcc16068096a9d32eb0198bfc76e41b5
leeminsung0098 <lminsung98@gmail.com>
2023-05-26 15:20:37 +0900

    커밋 테스트

--- day2/Loof/Loof3.java
+++ day2/Loof/Loof3.java
@@ -3,8 +3,19 @@ package day2.Loof;
 public class Loof3 {
     public static void main(String[] args) {
 
-        //구구단
-        System.out.printf("2 x 1 = 2");
+        String hell = "Hello World";
+        char[] chars = hell.toChrArray();
+
+        for (int i = 0 ; i < chars.length; i++){
+            char ch = hell[i];
+            if (ch >= 'A' && ch <= 'Z'){
+                chars[i] = (char) ( ch- 'a'+'A');
+            } else if (ch >= 'A'&& ch<= 'Z'){
+                chars
+            }
+        }
+
+
 
 
 
