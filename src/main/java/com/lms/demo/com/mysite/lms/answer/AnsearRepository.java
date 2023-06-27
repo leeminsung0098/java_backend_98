66fbab5e2842426a476611ee14f4f3ac5667376e
leeminsung0098 <lminsung98@gmail.com>
2023-06-27 11:36:23 +0900

    aaa

---
+++ src/main/java/com/lms/demo/com/mysite/lms/answer/AnsearRepository.java
@@ -0,0 +1,7 @@
+package com.lms.demo.com.mysite.lms.answer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnsearRepository extends JpaRepository<Answear ,Integer> {
+
+}
