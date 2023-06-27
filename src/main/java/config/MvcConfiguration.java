c1433995e874da90e71ab58fdbd027e509b3de73
leeminsung0098 <lminsung98@gmail.com>
2023-06-27 12:35:58 +0900

    er

---
+++ src/main/java/config/MvcConfiguration.java
@@ -0,0 +1,15 @@
+package config;
+
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
+
+public class MvcConfiguration implements WebMvcConfigurer {
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/templates/","classpath:/static/")
+                .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
+    }
+}
